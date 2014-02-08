a856a36b3592bab4bd69cd7551600ac86d18554c
Anthony Caiafa <anthony@digitalocean.com>
2014-02-07 19:05:04 -0500

    adding supports

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ description      "Installs/Configures memcached"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.1.0"
 name            "memcached"
+
+supports 'centos'
+supports 'redhat'
+supports 'fedora'
