62758f87794c44c13fe0d7153b07d407993531d8
Anthony Caiafa <anthony@digitalocean.com>
2014-02-10 13:12:55 -0500

    moving version to 1.0 and creating tag

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "2600.ac@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures memcached"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.1.0"
+version "1.0.0"
 name            "memcached"
 
 supports 'centos'
