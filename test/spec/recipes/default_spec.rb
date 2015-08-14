521928b1116b840079ab0d412a277edb609313e8
Anthony Caiafa <2600.ac@gmail.com>
2015-08-14 11:44:25 -0400

    updating test

--- test/spec/recipes/default_spec.rb
+++ test/spec/recipes/default_spec.rb
@@ -4,8 +4,6 @@ describe_recipe 'memcached::default' do
   cached(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
   context 'with default attributes' do
-    it { expect(chef_run).to enable_memcached_instance('memcached-test') }
-
     it 'converges successfully' do
       chef_run
     end
